1e2f256d657c7f0bde1ada9f2c731f7ed6d2a349
T3NED <t3ned.root@gmail.com>
2021-04-10 15:43:51 +0100

    ✨ Add guild getters

--- src/lib/struct/Client.ts
+++ src/lib/struct/Client.ts
@@ -14,7 +14,7 @@ export class Client extends AkairoClient {
 
   public db = new Database();
 
-  async login() {
+  public async login() {
     this.listenerHandler.setEmitters({
       commandHandler: this.commandHandler,
       listenerHandler: this.listenerHandler
@@ -27,10 +27,32 @@ export class Client extends AkairoClient {
     await this.db.init();
     return super.login();
   }
+
+  /**
+   * Resolves the root guild
+   * @returns The root guild
+   */
+  public get rootGuild() {
+    const guild = this.guilds.cache.get(process.env.ROOT_SERVER_ID!);
+    if (!guild) throw new Error("Failed to resolve root server.");
+    return guild;
+  }
+
+  /**
+   * Resolves the inbox guild
+   * @returns The inbox guild
+   */
+  public get inboxGuild() {
+    const guild = this.guilds.cache.get(process.env.INBOX_SERVER_ID!);
+    if (!guild) return this.rootGuild;
+    return guild;
+  }
 }
 
 declare module "discord.js" {
   export interface Client {
     readonly db: Database;
+    readonly rootGuild: Guild;
+    readonly inboxGuild: Guild;
   }
 }
