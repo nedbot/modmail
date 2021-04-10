c733dfd88c2c6c6e496bcad8a2d58ab32dcb6c4e
T3NED <t3ned.root@gmail.com>
2021-04-10 21:32:16 +0100

    ✨ Add constants

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export * from "./struct/Client";
 export * from "./struct/Thread";
 export * from "./util/Functions";
+export * as Constants from "./util/Constants";
---
+++ src/lib/util/Constants.ts
@@ -0,0 +1,4 @@
+export enum Colors {
+  ThreadMessage = 0x0a7c7e7,
+  UserMessage = 0x77dd77
+}
